50bef8a2262c22ec5b1ca27ed8c8f7c30574c762
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-09-12 22:42:24 +0700

    Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
 import { publicRouter } from './routes';
+import DefaultLayout from './components/Layout/DefaultLayout';
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                {publicRouter.map((router,index) =>{
+                {
+                publicRouter.map((router,index) =>{
                     const Page = router.component
                     return <Route key={index} path={router.path} element={<Page/>} />
                 })}     
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import Home from '../pages/Home'
 import Following from '../pages/Following'
+import Profile from '../pages/Profile'
 // Public
 const publicRouter = [ 
     { path:'/', component: Home},
-    { path:'/Following', component: Following},
+    { path:'/following', component: Following},
+    { path:'/profile', component: Profile},
 ]
 const privateRouter = [ 
 
