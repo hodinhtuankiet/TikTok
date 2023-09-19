82b27e03c8444577ded2e38f2ec338d67c4f8c09
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-09-19 20:57:21 +0700

    built frame

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,16 @@
 import Header from '../components/Header'
 import Sidebar from './Sidebar/index.js'
+import styles from './DefaultLatout.modules.scss'
+import classNames from 'classnames';
+const cx = classNames.bind(styles)
 
 function DefaultLayout({children}) {
     return ( 
-        <div>
+        <div className={cx('wrapper')}>
             <Header/>
-            <div className='container'>
+            <div className={cx('container')}>
                 <Sidebar/>
-                <div className='content'>
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
